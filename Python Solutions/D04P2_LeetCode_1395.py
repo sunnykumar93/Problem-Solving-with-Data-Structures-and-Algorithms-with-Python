72aa180b7a3b5c6033ca28628d464c8568c628ed
sunnykumar93 <86824238+sunnykumar93@users.noreply.github.com>
2023-10-20 07:10:20 -0400

    Update D04P2_LeetCode_1395.py
    Count Number of Teams

--- Python Solutions/D04P2_LeetCode_1395.py
+++ Python Solutions/D04P2_LeetCode_1395.py
@@ -1,11 +1,34 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
-        count=0
-        for  i in range (len(rating)):
-            for j in range(i+1,len(rating)):
-                for k in range(j+1,len(rating)):
-                    if (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]):
-                        count+=1
-        return count
+        if len(rating) < 3:
+            return 0
+        n=len(rating)
+        left=[0]
+        right=[0]
+        for i in range(1,n):
+            count=0
+            for j in range(0,i):
+                if rating[i]>rating[j]:
+                    count+=1
+            left.append(count)
+        
+        for i in range(n-2,-1,-1):
+            count=0
+            for j in range(n-1,i,-1):
+                if rating[i]>rating[j]:
+                    count+=1
+            right.insert(0,count)
+        
+        res=0
+        for i in range(1,n):
+            for j in range(0,i):
+                if rating[i]>rating[j]:
+                    res+=left[j]
+        
+        for i in range(n-2,-1,-1):
+            for j in range(n-1,i,-1):
+                if rating[i]>rating[j]:
+                    res+=right[j]
+        return res
                     
                     
