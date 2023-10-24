a2b0307f2252dd04455bf0d470eef42d76d183c2
sunnykumar93 <86824238+sunnykumar93@users.noreply.github.com>
2023-10-24 12:10:15 +0530

    Update D08P3_LeetCode_53.py
    Maximum Subarray

--- Python Solutions/D08P3_LeetCode_53.py
+++ Python Solutions/D08P3_LeetCode_53.py
@@ -1,14 +1,12 @@
-#Given an integer array nums, find the contiguous subarray (containing at least one number) 
-#which has the largest sum and return its sum
-
 class Solution:
-  #Kadane's Algorithm
-  def maxSubArray(self, nums: List[int]) -> int:
-      max_sum = nums[0]
-      cur_sum = max_sum
-      
-      for i in range(1, len(nums)):
-          cur_sum = max(nums[i] + cur_sum, nums[i])
-          max_sum = max(cur_sum, max_sum)
-      
-      return max_sum
+    def maxSubArray(self, nums: List[int]) -> int:
+        left=[]
+        m=nums[0]
+        s=0
+        for i in range(len(nums)):
+            s=s+nums[i]
+            m=max(s,m)
+            if s<0:
+                s=0
+            
+        return m
