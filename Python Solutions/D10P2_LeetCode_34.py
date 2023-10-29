2c104fcc97bec9992349db5f15f3c7d6a1b0fdf4
sunnykumar93 <86824238+sunnykumar93@users.noreply.github.com>
2023-10-29 02:26:02 -0400

    Update D10P2_LeetCode_34.py
    First and last position of element

--- Python Solutions/D10P2_LeetCode_34.py
+++ Python Solutions/D10P2_LeetCode_34.py
@@ -1,21 +1,30 @@
-# Find First and Last Position of Element in Sorted Array
-class Solution(object):
-    def searchRange(self, nums, target):
-        if len(nums) == 0: 
-            return([-1, -1])
-        low_element, high_element = 0, len(nums)
-        while low_element < high_element:
-            mid_element = (high_element + low_element) // 2
-            if nums[mid_element] == target: 
-                left_element, right_element = mid_element, mid_element
-                while (left_element-1) >= 0 and nums[left_element-1] == target: 
-                    left_element -= 1
-                while (right_element+1) < len(nums) and nums[right_element+1] == target: 
-                    right_element += 1
-                return([left_element, right_element])
-                break
-            elif nums[mid_element] < target: 
-                low_element = mid_element + 1
-            elif nums[mid_element] > target: 
-                high_element = mid_element
-        return([-1, -1])
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        l=[-1,-1]
+        low=0
+        high=len(nums)-1
+        low_bound=len(nums)
+        h_bound=len(nums)
+        if target not in nums:
+            return l
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]>=target:
+                low_bound=mid
+                high=mid-1
+            else:
+                low=mid+1
+
+        print(low_bound)
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]>target:
+                h_bound=mid
+                high=mid-1
+            else:
+                low=mid+1
+        print(h_bound-1)
+        l=[low_bound,h_bound-1]
+        return l
