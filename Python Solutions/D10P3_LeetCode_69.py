edb8af46dc41f10dc1dd3d0c40f1e88540b175c1
sunnykumar93 <86824238+sunnykumar93@users.noreply.github.com>
2023-10-29 01:10:04 -0400

    Update D10P3_LeetCode_69.py
    square root of number

--- Python Solutions/D10P3_LeetCode_69.py
+++ Python Solutions/D10P3_LeetCode_69.py
@@ -1,7 +1,15 @@
-# Sqrt(x)
-class Solution(object):
-    def mySqrt(self, x):
-        # return(int(x ** 0.5))
-        import math
-        return(int(math.sqrt(x)))
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l=0
+        h=x
+        res=0
+        while l<=h:
+            m=(l+h)//2
+            if m*m>x:
+                h=m-1
+            elif m*m<=x:
+                res=m
+                l=m+1
+           
+        return res
         
