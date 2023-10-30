8542e8b9f72fd4604fe35f8bd508ba52614a467f
sunnykumar93 <86824238+sunnykumar93@users.noreply.github.com>
2023-10-30 02:15:43 -0400

    Update D11P1_LeetCode_162.py
    Find Peak Element

--- Python Solutions/D11P1_LeetCode_162.py
+++ Python Solutions/D11P1_LeetCode_162.py
@@ -1,4 +1,21 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-         return nums.index(max(nums))
-        
+        low=0
+        high=len(nums)-1
+        if len(nums)==1:
+            return 0
+        if nums[0]>nums[1]:
+            return 0
+        if nums[len(nums)-1]>nums[len(nums)-2]:
+            return len(nums)-1
+        ans=0
+        while(low<=high):
+            mid=(low+high)//2
+            if nums[mid-1]<nums[mid]>nums[mid+1]:
+                ans=mid
+                break
+            elif nums[mid]<nums[mid+1]:
+                low=mid+1
+            elif nums[mid-1]>nums[mid]:
+                high=mid-1
+        return ans
