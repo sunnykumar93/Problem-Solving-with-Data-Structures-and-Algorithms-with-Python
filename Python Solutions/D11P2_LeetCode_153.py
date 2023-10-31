e45cba185086a9648b40d0f6b9833bb4ba725cb0
sunnykumar93 <86824238+sunnykumar93@users.noreply.github.com>
2023-10-31 02:03:20 -0400

    Update D11P2_LeetCode_153.py
    Find Minimun in  rotated sorted array

--- Python Solutions/D11P2_LeetCode_153.py
+++ Python Solutions/D11P2_LeetCode_153.py
@@ -1,15 +1,19 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        left = 0
-        right = len(nums) - 1
-
-        while (left < right):
-            midpoint = int(left + (right - left)/2)
-            if(nums[midpoint] < nums[midpoint -1]):
-                return nums[midpoint]
-            elif (nums[midpoint] >= nums[left] and nums[midpoint] > nums[right]):
-                left = midpoint+1
+        low=0
+        high=len(nums)-1
+        ans=float("inf")
+        if len(nums)==1:
+            return nums[0]
+        
+        while low<=high:
+            mid=(low+high)//2
+            if nums[low]<=nums[mid]:
+                ans=min(ans,nums[low])
+                low=mid+1
             else:
-                right = midpoint - 1
-
-        return nums[left]
+                ans=min(ans,nums[mid])
+                high=mid-1
+            
+               
+        return ans
